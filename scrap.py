db6fe4c56ee1f050a4aa184f340cef5cb8e77aec
Salik Sadruddin <ssm14293@gmail.com>
2020-06-09 13:51:29 +0500

    updated logic

--- scrap.py
+++ scrap.py
@@ -11,27 +11,43 @@ tags = set()
 platforms = set()
 
 # Free to play games
-with open("./games.json", "w") as fp:
+with open("./topsellers.json", "w") as fp:
   while(items < total):
+    print("Sending request for ", items, "items")
     data = requests.get("https://store.steampowered.com/contenthub/querypaginated/games/TopSellers/render/", {"query": "", "start": items, "count": 15, "cc": "PK", "l":"english", "v":"4", "tag": ""}).json()
-    # data = requests.get("https://store.steampowered.com/contenthub/querypaginated/freetoplay/TopSellers/render/", {"query": "", "start": items, "count": 15, "cc": "PK", "l":"english", "v":"4", "tag": ""}).json()
     items += 15
     total = data["total_count"]
+    print("Total items:", total)
     soup = BeautifulSoup(data["results_html"], 'html.parser')
+    print("Looping anchors")
     for g in soup.find_all("a", recursive=False):
       game = dict()
       game["name"] = g.find("div", class_="tab_item_name").text
       game["platforms"] = [plat for x in g.find_all("span", class_="platform_img") for plat in x["class"] if plat != "platform_img"]
-      game["tags"] = set({x.text.replace(" ", "").replace(",", "").lower() for x in g.find_all("span", class_="top_tag")})
-      tags.union(game["tags"])
+      t = set({x.text.replace(" ", "").replace(",", "").lower() for x in g.find_all("span", class_="top_tag")})
+      tags.union(t)
+      game["tags"] = list(t)
       platforms.union(set(game["platforms"]))
-      try:
+      discount = g.find("div", class_="discount_pct")
+      game["discount"] = float(discount.text.replace("-", "").replace("%", "")) if discount != None  else 0
+      price_raw = g.find("div", class_="discount_final_price")
+      if price_raw == None:
+        game["price"] = 0
+        game["original_price"] = 0
+      elif "free" in price_raw.text.lower():
+        game["price"] = 0
+        game["original_price"] = 0
+      elif game["discount"] == 0:
         price_raw = g.find("div", class_="discount_final_price").text
-        price = price_raw if "free" not in price_raw.lower() else "$0"
-        game["price"] = float(price[1:])
-      except:
-        print(items / 15)
-        game["price"] = 0.0
+        game["price"] = float(price_raw.replace("$", ""))
+        game["original_price"] = game["price"]
+      else:
+        price_raw = g.find("div", class_="discount_final_price").text
+        game["price"] = float(price_raw.replace("$", ""))
+        game["original_price"] = float(g.find("div", class_="discount_original_price").text.replace("$", ""))
+      print("adding game to list")
       games.append(game)
-    time.sleep(5)
-  json.dump({"games": games, "tags": tags}, fp)
+    print("Entering Sleep to avoid getting banned by server")
+    time.sleep(10)
+  print("Dumping JSON")
+  json.dump({"games": games, "tags": list(tags), "platforms": list(platforms)}, fp)
